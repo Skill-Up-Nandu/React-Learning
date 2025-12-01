20139f7942c69473f5aa7dfbb901e9425ea89b15
Skill-Up-Nandu <behindthescenesgrowth@gmail.com>
2025-12-01 19:58:42 +0530

    React routing using navbar

--- react_dom_rout/src/App.jsx
+++ react_dom_rout/src/App.jsx
@@ -3,19 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import { Routes, Router } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import About from './pages/About'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Contact from './pages/Contact'
+import PageNotFound from './pages/PageNotFound'
 
 const App=()=>{
 
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </>
   )
--- react_dom_rout/src/components/Navbar.jsx
+++ react_dom_rout/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import Link from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
     return (
     
-        <div>
+        <div className='navbar'>
             <h1>
                 Upskill Nandu
             </h1>
-            <Link>Home</Link>
-            <Link>About</Link>
-            <Link>Contact</Link>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+            
 
         </div>
     )
